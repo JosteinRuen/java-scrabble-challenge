2d6fef9cb4d66190390fbaa5a90835c1e8ccc0e7
Jostein Ruen <josteinruen@gmail.com>
2024-08-13 15:12:49 +0200

    Jostein Ruen

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,72 @@
 package com.booleanuk;
 
-public class Scrabble {
-    public Scrabble(String word) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class Scrabble {
+    HashMap<String, Integer> values;
+    String word;
+    int score;
+    public Scrabble(String word) {
+        //define the scrabble pointsystem
+        this.values = new HashMap<>();
+        this.word = word;
+        this.score = 0;
+        this.values.put("A",1);
+        this.values.put("E",1);
+        this.values.put("I",1);
+        this.values.put("O",1);
+        this.values.put("U",1);
+        this.values.put("L",1);
+        this.values.put("N",1);
+        this.values.put("R",1);
+        this.values.put("S",1);
+        this.values.put("T",1);
+        this.values.put("D",2);
+        this.values.put("G",2);
+        this.values.put("B",3);
+        this.values.put("C",3);
+        this.values.put("M",3);
+        this.values.put("P",3);
+        this.values.put("F",4);
+        this.values.put("H",4);
+        this.values.put("V",4);
+        this.values.put("W",4);
+        this.values.put("Y",4);
+        this.values.put("K",5);
+        this.values.put("J",8);
+        this.values.put("X",8);
+        this.values.put("Q",10);
+        this.values.put("Z",10);
     }
 
     public int score() {
-        return -1;
+        ArrayList<String> lettersInWord = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++){
+            String character = String.valueOf(word.toUpperCase().charAt(i));
+
+            System.out.println(character);
+
+            if(!values.containsKey(character)){
+                //do nothing
+            }else{
+                lettersInWord.add(character);
+            }
+        }
+
+        if(lettersInWord.isEmpty()){
+            return 0;
+        }else{
+
+            for(String letter: lettersInWord) {
+                score += values.get(letter);
+            }
+
+        }
+        return score;
     }
+
+
 
 }
